84be5b70989ee5116b9ace375a7f8abd25aec932
Godofredo <gsandovalramirez@gmail.com>
2023-05-17 08:23:31 -0500

    commit inicial

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<div id="footer">
+
+<!-- Copyright -->
+    <ul class="copyright">
+        <li>&copy; Todo los derechos reservados</li><li>Gsr: <a href="">HTML5 UP</a></li>
+    </ul>
+
+</div>
